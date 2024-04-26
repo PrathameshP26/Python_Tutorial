5e38be0b53e7b5c44f8bf9c8395d7021576f6c74
PrathameshP26 <150712231+PrathameshP26@users.noreply.github.com>
2024-04-26 19:22:28 +0530

    parameterized constructor

--- Constructors.py
+++ Constructors.py
@@ -1,6 +1,15 @@
 class Person:
-    def __init__(self) -> None:
+    def __init__(self,n,o) -> None:
         print("hey")
+        self.name=n
+        self.occ=o
+    
+    def display(self):
+        print("name is ",self.name)
+        print(f"occupation of {self.name} is {self.occ}")
 
-a=Person()
-b=Person()
+a=Person("ram","ds")
+a.display()
+
+b=Person("raj","da")
+b.display()
