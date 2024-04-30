f3576886942ede99edb037abaed5397913a53877
PrathameshP26 <150712231+PrathameshP26@users.noreply.github.com>
2024-04-30 10:59:01 +0530

    decorators with parameters

--- decorators.py
+++ decorators.py
@@ -15,4 +15,20 @@ decorator will modify the function given below and return the modified function
 
 '''
 
+def greet_math(func):
+    def math_func(*args):
+        print("Welcome to math lecture!!")
+        func(*args)
+    return math_func
+        
 
+@greet_math
+def add(a,b):
+    print(a+b)
+
+@greet_math
+def sub(a,b):
+    print(a-b)
+
+add(2,3)
+sub(7,5)
