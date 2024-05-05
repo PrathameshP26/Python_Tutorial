69df14e119be5db1804559b84faf501336b802d9
PrathameshP26 <150712231+PrathameshP26@users.noreply.github.com>
2024-05-05 11:56:15 +0530

    class methods as constructors

--- class_methods_as_constructors.py
+++ class_methods_as_constructors.py
@@ -7,5 +7,13 @@ class Person:
     def nameAge(self):
         return f"The name of the person is {self.name} and age is {self.age}."
     
+    @classmethod
+    def from_string(self,str1):
+        return self(str1.split("-")[0],str1.split("-")[1])
+
 p1 =Person("ram",23)
 print(p1.nameAge)
+
+str="ajay-24"
+p2 = Person.from_string(str)
+print(p2.nameAge)
