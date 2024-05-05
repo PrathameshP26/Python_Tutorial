180e52ea30a43b5f8b847c552087756a9929bd05
PrathameshP26 <150712231+PrathameshP26@users.noreply.github.com>
2024-05-05 11:28:08 +0530

    class method done!

---
+++ class_methods.py
@@ -0,0 +1,23 @@
+class Person:
+    name="henry"
+
+    def __init__(self) -> None:
+        pass
+
+    @classmethod
+    def changeName(self,name):
+        self.name=name
+    
+    @property
+    def showName(self):
+        return self.name
+    
+p1 =Person()
+print(p1.showName)
+
+p1.changeName("robin")
+
+print(p1.showName)
+print("the class variable is ",Person.name)
+
+# class mathod is used to access the  class variables and is used to modify them . @ classmethod  decorator is used to make a method as class method.Otherwise the method will change only the instance variable.
