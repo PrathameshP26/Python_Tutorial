84b8aa34e832384502b563b32f5f903a61bd4ccc
PrathameshP26 <150712231+PrathameshP26@users.noreply.github.com>
2024-05-05 11:29:01 +0530

    Output of classmethod.py

--- class_methods.py
+++ class_methods.py
@@ -21,3 +21,12 @@ print(p1.showName)
 print("the class variable is ",Person.name)
 
 # class mathod is used to access the  class variables and is used to modify them . @ classmethod  decorator is used to make a method as class method.Otherwise the method will change only the instance variable.
+'''
+Output:
+
+PS D:\Python_Tutorial> python -u "d:\Python_Tutorial\class_methods.py"
+henry
+robin
+the class variable is  robin
+PS D:\Python_Tutorial>
+'''
