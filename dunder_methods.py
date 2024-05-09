bfe07539c95458ecd44b1284db6dcabeee07e0de
PrathameshP26 <150712231+PrathameshP26@users.noreply.github.com>
2024-05-09 19:57:08 +0530

    __str__ and __repr__ methods

--- dunder_methods.py
+++ dunder_methods.py
@@ -12,8 +12,16 @@ class Employee:
 
     def __call__(self, *args: Any, **kwds: Any) -> Any:
         print("hey i am good")
+    
+    '''
+    def __str__(self) -> str:
+        return f"{self.name} is a good boy"
+    '''
+    def __repr__(self) -> str:
+        return f"{self.name} is a good man"
 
 e1 = Employee()
 print(e1.name)
 print(len(e1))
 e1()
+print(e1)
