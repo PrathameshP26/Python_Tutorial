b4e7fffce7d77d4a017497f46bc332ed2fc12571
PrathameshP26 <150712231+PrathameshP26@users.noreply.github.com>
2024-05-09 18:47:53 +0530

    __dict__ method

--- dir,__dict__,help.py
+++ dir,__dict__,help.py
@@ -1,2 +1,10 @@
 x=[1,2,3,4]
 print(dir(x))
+
+class A:
+    def __init__(self) -> None:
+        self.name="ram"
+        self.place="ayodhya"
+    
+a=A()
+print(a.__dict__) # all the instance variables are now in dictionary format
